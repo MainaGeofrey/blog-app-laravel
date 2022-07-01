1c8c3417f272d06746c55906881ffe964aa81737
jeff <mainajfry@gmail.com>
2022-07-01 15:22:44 +0300

    blog api with laravel sanctum auth

---
+++ app/Http/Resources/BlogResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BlogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'image' => $this->image,
+            'content' => $this->content,
+            'user_id' => $this->user_id,
+            'user_name' => $this->user->name,
+            'user_email' => $this->user->email,
+            'created_at' => $this->created_at->format('m/d/Y'),
+            'updated_at' => $this->updated_at->format('m/d/Y'),
+        ];
+
+    }
+}
